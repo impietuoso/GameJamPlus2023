1ac0f7375c3887002244fb4f6d71ebbf7747cde8
Impietuoso <sauldesigner.ce@gmail.com>
2023-10-21 16:14:28 -0300

    Ajustes de BLocos e Blocos de Impulso e Lentidão

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -42,25 +42,18 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "EspulsionBlock") {
+            col.gameObject.GetComponent<EspulsionBlock>().Espulsion(rb);
+        }
+        if (col.gameObject.tag == "JumpBlock") {
+            col.gameObject.GetComponent<JumpBlock>().Jump(rb);
+        }
+    }
+
     private void OnCollisionStay(Collision col) {
         if(col.gameObject.tag == "SpeedBlock") {
-            float forcePush = col.gameObject.GetComponent<SpeedBlock>().force;
-            Vector3 forceDir = Vector3.zero;
-            switch (col.transform.rotation.y) {
-                case 0:
-                    forceDir = new Vector3(0, 0, 1);
-                    break;
-                case 90:
-                    forceDir = new Vector3(1, 0, 0);
-                    break;
-                case 180:
-                    forceDir = new Vector3(0, 0, -1);
-                    break;
-                case 270:
-                    forceDir = new Vector3(-1, 0, 0);
-                    break;
-            }
-            rb.AddForce(forceDir * forcePush, ForceMode.VelocityChange);
+            col.gameObject.GetComponent<SpeedBlock>().Speed(rb);
         }
     }
 }
